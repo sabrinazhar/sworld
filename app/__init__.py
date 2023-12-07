8eae369a7b634028f2f4cccddf1577744fc913ab
sabrinazhar <sc22nsbm@leeds.ac.uk>
2023-12-07 14:03:05 +0000

    migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 " init.py that contains the variables to be initialised"
 
-from flask import Flask
+from flask import Flask, app
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
 from config import Config
 
 app = Flask(__name__, template_folder='templates')
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
--- flask/Scripts/activate.bat
+++ flask/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\user\OneDrive - University of Leeds\FALL 2023\COMP2011\social_media\flask
+set VIRTUAL_ENV=C:\Users\user\OneDrive - University of Leeds\FALL 2023\COMP2011\sworld\flask
 
 if not defined PROMPT set PROMPT=$P$G
 
